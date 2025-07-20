ee43688915beb3fbca230e3bae7a7bc4d29559e6
rohit@ROHIT <rohitsingh95724@gmail.com>
2025-07-20 18:20:48 +0530

    frontend deployment fix

--- test/vite.config.js
+++ test/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
